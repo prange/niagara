b1f3358f89a57bac86d3fbe289c0b2e3c204b65d
Atle Prange <atle.prange@gmail.com>
2019-04-18 01:41:04 +0200

    Added http service builder

---
+++ niagara-http/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.bundling.Jar
+
+plugins {
+    maven
+    `maven-publish`
+    java
+    kotlin("jvm")
+}
+
+dependencies {
+    compile(project(":niagara-data"))
+    compile(project(":niagara-json"))
+    compile("io.vavr:vavr:0.10.0")
+    compile("io.vavr:vavr-kotlin:0.10.0")
+    compile("io.undertow:undertow-core:2.0.1.Final")
+    compile(kotlin("stdlib-jdk8"))
+}
+
+
+
+
+repositories {
+    mavenCentral()
+}
+val compileKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    from(sourceSets["main"].allSource)
+}
