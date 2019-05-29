de10b8f264d1b6b90de082181a1a90d5464078cd
Atle Prange <atle.prange@gmail.com>
2019-05-29 21:41:29 +0200

    Some more products

--- niagara-data/src/main/kotlin/org/kantega/niagara/data/Product.kt
+++ niagara-data/src/main/kotlin/org/kantega/niagara/data/Product.kt
@@ -3,18 +3,56 @@ package org.kantega.niagara.data
 
 interface Product1<A>{
     fun component1():A
-    fun hList():HList1<A> = hList(component1())
+    fun hList() = hList(component1())
 }
 
 interface Product2<A,B>{
     fun component1():A
     fun component2():B
-    fun hList():HList2<A,B> = hList(component1(),component2())
+    fun hList() = hList(component1(),component2())
 }
 
 interface Product3<A,B,C>{
     fun component1():A
     fun component2():B
     fun component3():C
-    fun hList():HList3<A,B,C> = hList(component1(),component2(),component3())
+    fun hList() = hList(component1(),component2(),component3())
+}
+
+interface Product4<A,B,C,D>{
+    fun component1():A
+    fun component2():B
+    fun component3():C
+    fun component4():D
+    fun hList() = hList(component1(),component2(),component3(),component4())
+}
+
+interface Product5<A,B,C,D,E>{
+    fun component1():A
+    fun component2():B
+    fun component3():C
+    fun component4():D
+    fun component5():E
+    fun hList() = hList(component1(),component2(),component3(),component4(),component5())
+}
+
+interface Product6<A,B,C,D,E,F>{
+    fun component1():A
+    fun component2():B
+    fun component3():C
+    fun component4():D
+    fun component5():E
+    fun component6():F
+    fun hList() = hList(component1(),component2(),component3(),component4(),component5(),component6())
+}
+
+interface Product7<A,B,C,D,E,F,G>{
+    fun component1():A
+    fun component2():B
+    fun component3():C
+    fun component4():D
+    fun component5():E
+    fun component6():F
+    fun component7():G
+    fun hList() = hList(component1(),component2(),component3(),component4(),component5(),component6(),component7())
 }
--- niagara-data/src/main/kotlin/org/kantega/niagara/data/products.kt
+++ niagara-data/src/main/kotlin/org/kantega/niagara/data/products.kt
@@ -10,12 +10,14 @@ typealias P2<A,B> = Tuple2<A,B>
 typealias P3<A,B,C> = Tuple3<A,B,C>
 typealias P4<A,B,C,D> = Tuple4<A,B,C,D>
 typealias P5<A,B,C,D,E> = Tuple5<A,B,C,D,E>
+typealias P6<A,B,C,D,E,F> = Tuple6<A,B,C,D,E,F>
 
 fun <A> p(a:A) = tuple(a)
 fun <A,B> p(a:A,b:B) = tuple(a,b)
 fun <A,B,C> p(a:A,b:B,c:C) = tuple(a,b,c)
 fun <A,B,C,D> p(a:A,b:B,c:C,d:D) = tuple(a,b,c,d)
 fun <A,B,C,D,E> p(a:A,b:B,c:C,d:D,e:E) = tuple(a,b,c,d,e)
+fun <A,B,C,D,E,F> p(a:A,b:B,c:C,d:D,e:E,f:F) = tuple(a,b,c,d,e,f)
 
 fun <A,B,C> Tuple2<A,Tuple2<B,C>>.flatten():Tuple3<A,B,C> =
   Tuple.of(_1,_2._1,_2._2)
