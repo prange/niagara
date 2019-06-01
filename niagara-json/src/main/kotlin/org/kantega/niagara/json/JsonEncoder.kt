71bb46ae93f65f4ff05bd1a444a3930971f2b8be
Atle Prange <atle.prange@gmail.com>
2019-06-01 12:18:21 +0200

    Spacing...

--- niagara-json/src/main/kotlin/org/kantega/niagara/json/JsonEncoder.kt
+++ niagara-json/src/main/kotlin/org/kantega/niagara/json/JsonEncoder.kt
@@ -12,44 +12,46 @@ fun <A, B> JsonEncoder<B>.comap(f: (A) -> B): JsonEncoder<A> = { a ->
 }
 
 
-data class JsonObjectBuilder<A,B>(val f:(JsonObject, A)->P2<JsonObject,B>) : JsonEncoder<A> {
+data class JsonObjectBuilder<A, B>(val f: (JsonObject, A) -> P2<JsonObject, B>) : JsonEncoder<A> {
     override fun invoke(a: A): JsonValue =
-        f(JsonObject(),a)._1
+      f(JsonObject(), a)._1
 
-    fun <C> append(f:(JsonObject,B)->P2<JsonObject,C>):JsonObjectBuilder<A,C> = JsonObjectBuilder{jsonObject, a ->
-        val (nextObj,b) = f(jsonObject,a)
-        f(nextObj,b)
+    fun <C> append(f: (JsonObject, B) -> P2<JsonObject, C>): JsonObjectBuilder<A, C> = JsonObjectBuilder { jsonObject, a ->
+        val (nextObj, b) = f(jsonObject, a)
+        f(nextObj, b)
     }
 }
 
 
-fun <A,B> encode(destructor: (A) -> HCons<B,HList>): JsonObjectBuilder<A,HCons<B,HList>> =
-  JsonObjectBuilder({ jsonObj, a->p(jsonObj,destructor(a))})
+fun <A, B, T : HList> encode(destructor: (A) -> HCons<B, T>): JsonObjectBuilder<A, HCons<B, T>> =
+  JsonObjectBuilder({ jsonObj, a -> p(jsonObj, destructor(a)) })
 
-fun <A, B, T, L : HCons<B,T>> JsonObjectBuilder<A, L>.field(name: String, aEncoder: JsonEncoder<B>): JsonObjectBuilder<A, T> = append{ jsonObj, hList ->
-    p(jsonObj.set(name, aEncoder(hList.head)), hList.tail)
-}
+fun <A, B, T, L : HCons<B, T>> JsonObjectBuilder<A, L>.field(name: String, aEncoder: JsonEncoder<B>): JsonObjectBuilder<A, T> =
+  append { jsonObj, hList ->
+      p(jsonObj.set(name, aEncoder(hList.head)), hList.tail)
+  }
 
-fun <A,  L : HList> JsonObjectBuilder<A, L>.value(name: String, value: JsonValue): JsonObjectBuilder<A, L> = append{ jsonObj, hList ->
-    p(jsonObj.set(name, value), hList)
-}
+fun <A, L : HList> JsonObjectBuilder<A, L>.value(name: String, value: JsonValue): JsonObjectBuilder<A, L> =
+  append { jsonObj, hList ->
+      p(jsonObj.set(name, value), hList)
+  }
 
 
-val encodeString:JsonEncoder<String> =::JsonString
+val encodeString: JsonEncoder<String> = ::JsonString
 
-val encodeNumber:JsonEncoder<BigDecimal> = ::JsonNumber
+val encodeNumber: JsonEncoder<BigDecimal> = ::JsonNumber
 
-val encodeInt:JsonEncoder<Int> = { n -> JsonNumber(n.toBigDecimal())}
+val encodeInt: JsonEncoder<Int> = { n -> JsonNumber(n.toBigDecimal()) }
 
-val encodeLong:JsonEncoder<Long> = { n -> JsonNumber(n.toBigDecimal())}
+val encodeLong: JsonEncoder<Long> = { n -> JsonNumber(n.toBigDecimal()) }
 
-val encodeDouble:JsonEncoder<Double> = { n -> JsonNumber(n.toBigDecimal())}
+val encodeDouble: JsonEncoder<Double> = { n -> JsonNumber(n.toBigDecimal()) }
 
-val encodeBool:JsonEncoder<Boolean> = ::JsonBool
+val encodeBool: JsonEncoder<Boolean> = ::JsonBool
 
-fun <A> encodeArray(elemEncoder:JsonEncoder<A>):JsonEncoder<List<A>> = {list ->
+fun <A> encodeArray(elemEncoder: JsonEncoder<A>): JsonEncoder<List<A>> = { list ->
     JsonArray(list.map { a -> elemEncoder(a) })
 }
 
 
-data class User(val name:String, val age:Int) : Product2<String, Int>
+data class User(val name: String, val age: Int) : Product2<String, Int>
